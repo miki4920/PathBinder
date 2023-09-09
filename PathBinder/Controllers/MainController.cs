6443b9908ae353ec7f36071c1d8d522095e95f18
miki4920 <miki4920@gmail.com>
2023-09-09 15:29:43 +0100

    Project Initialisation

---
+++ PathBinder/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PathBinder.Models;
+using System.Diagnostics;
+
+namespace PathBinder.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Write()
+        {
+            return View();
+        }
+
+        public IActionResult Library()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PathBinder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PathBinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
