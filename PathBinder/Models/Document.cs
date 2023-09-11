bd682cbd44392a22f27c085f36e97a4726850b70
miki4920 <miki4920@gmail.com>
2023-09-11 21:50:41 +0100

    Created basic document model

---
+++ PathBinder/Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PathBinder.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        [Required] 
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string documentText { get; set; } = string.Empty;
+    }
+}
