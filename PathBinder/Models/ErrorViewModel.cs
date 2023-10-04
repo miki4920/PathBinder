2123b4af3038d3869b910960ee6ceaffe8692593
miki4920 <miki4920@gmail.com>
2023-10-04 21:41:22 +0100

    Removed model files as we migrate to local-only storage

--- PathBinder/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PathBinder.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
