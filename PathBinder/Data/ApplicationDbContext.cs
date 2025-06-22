2e2cce454c8fad7109483d62481dc0c99853e194
miki4920 <miki4920@gmail.com>
2025-06-22 17:15:17 +0100

    Boilerplate

---
+++ PathBinder/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext
+    : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<FileItem> Files => Set<FileItem>();
+
+    protected override void OnModelCreating(ModelBuilder b)
+    {
+        base.OnModelCreating(b);
+        b.Entity<FileItem>()
+         .HasOne(f => f.User)
+         .WithMany()            
+         .HasForeignKey(f => f.UserId)
+         .OnDelete(DeleteBehavior.Cascade);
+        b.Entity<FileItem>()
+         .HasIndex(f => f.UserId);
+    }
+}
